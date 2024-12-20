2548cc1513a907449bb71dbc0b581a16fa8937b7
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-12-20 18:52:28 +0200

    Revert "Mostly remove FrameSequence"
    This reverts commit ea845e09e79a69c5601265c40dc169ad462c441a.
    
    libframesequence is still used by the Messaging app for GIF handling.

--- framesequence/jni/BitmapDecoderJNI.cpp
+++ framesequence/jni/BitmapDecoderJNI.cpp
@@ -14,8 +14,34 @@
  * limitations under the License.
  */
 
-#include <jni.h>
+#define LOG_TAG "FancyDecoding"
 
-jint JNI_OnLoad(JavaVM*, void*) {
+#include <android/bitmap.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "FrameSequenceJNI.h"
+#include "JNIHelpers.h"
+#include "Stream.h"
+#include "utils/log.h"
+
+void throwException(JNIEnv* env, const char* error) {
+    jclass clazz = env->FindClass("java/lang/RuntimeException");
+    env->ThrowNew(clazz, error);
+}
+
+jint JNI_OnLoad(JavaVM* vm, void* reserved) {
+    JNIEnv* env;
+    if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK) {
+        return -1;
+    }
+    if (FrameSequence_OnLoad(env)) {
+        ALOGE("Failed to load FrameSequence");
+        return -1;
+    }
+    if (JavaStream_OnLoad(env)) {
+        ALOGE("Failed to load JavaStream");
+        return -1;
+    }
+
     return JNI_VERSION_1_6;
 }
